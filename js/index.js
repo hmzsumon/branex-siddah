ef6d1cd4db22c873fe16746093bb27b6677242d5
Zakaria <zsumonn@gmail.com>
2022-01-16 21:35:23 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	autoplay: true,
+	autoplayTimeout: 2000,
+	autoplayHoverPause: true,
+	navText: ['Prev', 'Next'],
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 3,
+		},
+		1000: {
+			items: 5,
+		},
+	},
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				gold_dark: '#D7A837',
+				btn_gradient_border: 'linear-gradient(to right, red, blue)',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
